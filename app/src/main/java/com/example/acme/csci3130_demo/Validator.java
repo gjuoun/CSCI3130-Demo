17492244d9930a71c9744ab210a8f964e3482964
Jun <gjuoun@gmail.com>
2018-06-02 23:10:18 -0300

    added 3 more rules to Valicator.validate() method, also modified its test cases

--- app/src/main/java/com/example/acme/csci3130_demo/Validator.java
+++ app/src/main/java/com/example/acme/csci3130_demo/Validator.java
@@ -3,25 +3,29 @@ package com.example.acme.csci3130_demo;
 public class Validator {
     private String password;
 
-    public Validator(String password) {
-        this.password = password;
+    public Validator() {
     }
 
-    public boolean validate() {
+    public boolean validate(String password) {
+        this.password = password;
+
+        String atLeastOneLowerAndUpperCase = "(?=.*[a-z])(?=.*[A-Z]).{8,}";
+        String atLeastOneSpecialChar = "(?=.*[@#$%^&+=]).{8,}";
+        String atLeastOneDigit = "(?=.*[0-9]).{8,}";
+
 
         if(this.password.equals("password") )
             return false;
         else if(this.password.length() < 8)
             return false;
+        else if(!password.matches(atLeastOneLowerAndUpperCase))
+            return false;
+        else if(!password.matches(atLeastOneSpecialChar))
+            return false;
+        else if(!password.matches(atLeastOneDigit))
+            return false;
 
         return true;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- app/src/test/java/com/example/acme/csci3130_demo/ValidatorTest.java
+++ app/src/test/java/com/example/acme/csci3130_demo/ValidatorTest.java
@@ -2,26 +2,39 @@ package com.example.acme.csci3130_demo;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ValidatorTest {
     private static Validator validator;
 
     @BeforeClass
     public static void initClass(){
-        validator = new Validator("password");
+        validator = new Validator();
     }
 
     @Test
-    public void testValidate() {
-        assertFalse(validator.validate());
+    public void testValidateEasy() {
+        assertFalse(validator.validate("password"));
+        assertFalse(validator.validate("123442"));
+        assertFalse(validator.validate("Wnasd@"));
+        assertFalse(validator.validate("Ccccnn"));
+
     }
 
     @Test
-    public void testGetPassword() {
-        assertEquals("password", validator.getPassword());
+    public void testValidateMedium() {
+        assertFalse(validator.validate("Sccccccccccccc"));
+        assertFalse(validator.validate("Scc@ffffffffff"));
+
+        assertTrue(validator.validate("Ssddij2233@#$"));
+        assertTrue(validator.validate("Abcdefg$%#989"));
     }
+
 
 }
